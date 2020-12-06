6474330f1143ac021070989e774957bb7a190c60
alexander <acer080808@gmail.com>
2020-12-06 13:42:26 +0300

    prepare sbt project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# remittanceSystem
+# RemittanceSystem
+
+System requirements for compilation:
+JDK 1.8.0_201
+SBT 1.4.4
+
+Scala 2.13.4
---
+++ src/main/scala/ru/otus/remittance/App.scala
@@ -0,0 +1,9 @@
+package ru.otus.remittance
+
+object App {
+
+  def main(args: Array[String])  = {
+    println("Hrllow world")
+  }
+
+}
